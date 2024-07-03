cc308d3e77da968f06814cc2db466cbb7ff83de5
Srijan Shekhar <srijan.shekha@gmail.com>
2024-07-03 18:28:40 +0530

    spelling corrected

--- src/components/Tabs/TabsDiv.jsx
+++ src/components/Tabs/TabsDiv.jsx
@@ -4,7 +4,7 @@ import Tab from "./Tab.jsx";
 function tabsDiv() {
   return (
     <div className="tabsDiv">
-      <Tab to="/seat-availability" name="Seat Availibility" />
+      <Tab to="/seat-availability" name="Seat Availability" />
       <Tab to="/pnr-status" name="PNR Status" />
       <Tab to="/live-train-status" name="Live Train Status" />
     </div>
